44cde7b178eb5478cbd4cf507753a7f1b9ba2a8d
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-08-22 18:08:53 +0900

    Enable to post from form

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -29,9 +29,10 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-            {event.attendees.map((attendee, i) => (
-              <EventListAttendee key={attendee.id} attendee={attendee} />
-            ))}
+            {event.attendees &&
+              event.attendees.map((attendee, i) => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
           </List>
         </Segment>
         <Segment clearing>
