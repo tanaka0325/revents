6860103a55f8acd97f70751d638447d366d679fc
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-08-22 15:38:55 +0900

    Add EventForm Component

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Button } from "semantic-ui-react";
 
 import EventList from "../EventList/EventList";
+import EventForm from "../EventForm/EventForm";
+
 
 class EventDashboard extends Component {
   render() {
@@ -11,7 +13,8 @@ class EventDashboard extends Component {
           <EventList />
         </Grid.Column>
         <Grid.Column width={6}>
-          <h2>Right Column</h2>
+        <Button positive content='Create Events' />
+          <EventForm />
         </Grid.Column>
       </Grid>
     );
