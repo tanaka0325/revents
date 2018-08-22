62fc419c3923c0c19d0ebdc6e7b1d6fed72a6fc1
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-08-22 22:03:08 +0900

    Implement mock SettingsPages

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import SettingsNav from "./SettingsNav";
+import BasicPage from "../Settings/BasicPage";
+import AboutPage from "../Settings/AboutPage";
+import PhotosPage from "../Settings/PhotosPage";
+import AccountPage from "../Settings/AccountPage";
 
 const SettingsDashboard = () => {
   return (
-    <div>
-      <h1>SettingsDashboard</h1>
-    </div>
+    <Grid>
+      <Grid.Column width={12}>
+        <Switch>
+          <Redirect exact from="/settings" to="/settings/basic" />
+          <Route path="/settings/basic" component={BasicPage} />
+          <Route path="/settings/about" component={AboutPage} />
+          <Route path="/settings/photos" component={PhotosPage} />
+          <Route path="/settings/account" component={AccountPage} />
+        </Switch>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <SettingsNav />
+      </Grid.Column>
+    </Grid>
   );
 };
 
