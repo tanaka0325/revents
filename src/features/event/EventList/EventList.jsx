6f9e13b7a59ee468298c452930cce4e120a745e0
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-08-22 16:48:13 +0900

    Use sample state

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
 
-import EventListItem from './EventListItem'
+import EventListItem from "./EventListItem";
 
 class EventList extends Component {
   render() {
+    const { events } = this.props;
     return (
       <div>
-      <h1>EventList</h1>
-      <EventListItem />
-      <EventListItem />
-      <EventListItem />
+        <h1>EventList</h1>
+        {events.map(event => (
+          <EventListItem key={event.id} event={event} />
+        ))}
       </div>
     );
   }
 }
 
-export default EventList
+export default EventList;
