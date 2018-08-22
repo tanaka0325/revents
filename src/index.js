a95b8e22cb5d9e88a2fadfb79a9c485b33cadf76
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-08-22 13:28:18 +0900

    Enable to use hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
 registerServiceWorker();
