853727b2cc71a42575a5822ecf964c27af1d4ae9
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-08-22 14:50:47 +0900

    Add EventDashboardComponent

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+
 class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>Re-vents</h1>
+        <EventDashboard />
       </div>
     );
   }
